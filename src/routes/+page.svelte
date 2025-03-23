ecfbe2ba4b49f5ea161f556e76f76bfb7c044835
florobelle <zbnfloro@gmail.com>
2025-03-23 14:22:37 +0800

    feat: landing page cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,38 +17,21 @@
 		<div class="flex w-3/5 flex-col gap-8">
 			<div class="flex w-full flex-col gap-4 text-center">
 				<h1 class="text-5xl font-medium">
-					SUSê: A <span class="text-[#f48444]">Seamless</span>,
-					<span class="text-[#f48444]">Ubiquitous</span>, <span class="text-[#f48444]">Secure</span>
-					and <span class="text-[#f48444]">Efficient</span> system!
+					Asset Tracker and Management System
 				</h1>
-				<h2 class="text-lg font-normal">Choose the library you want to avail a service in.</h2>
+				<h2 class="text-lg font-normal">Choose the area you want to borrow an item in.</h2>
 			</div>
 			<div class="flex flex-row justify-center gap-2">
 				<Button
 					on:click={() => {
-						goto('/engglib1/circulation/auth/login');
-					}}>EnggLib I Circulation</Button
-				>
-				<Button
-					on:click={() => {
-						goto('/engglib1/ground-floor-services/auth/login');
-					}}>EnggLib I GFS</Button
-				>
-				<Button
-					on:click={() => {
-						goto('/engglib2/circulation/auth/login');
-					}}>EnggLib II Circulation</Button
-				>
-				<Button
-					on:click={() => {
-						goto('/engglib2/the-learning-commons/auth/login');
-					}}>EnggLib II TLC</Button
+						goto('/area/auth/login');
+					}}>Login</Button
 				>
 			</div>
 		</div>
 		<img
-			src="../../photos/landingpage.png"
-			class="m-auto mt-20 h-auto w-[90%] rounded-xl border border-slate-100 shadow-2xl xl:w-[68%]"
+			src="../../placeholder.png"
+			class="m-auto mt-20 h-80 object-none w-[90%] rounded-xl border border-slate-100 shadow-2xl xl:w-[68%]"
 			alt="service"
 		/>
 	</div>
