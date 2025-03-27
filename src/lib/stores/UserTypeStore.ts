4741379cc2b1552c251095819166c33db8789eff
florobelle <zbnfloro@gmail.com>
2025-03-27 13:35:19 +0800

    feat: user types

--- src/lib/stores/UserTypeStore.ts
+++ src/lib/stores/UserTypeStore.ts
@@ -1,7 +1,7 @@
 export const UserTypeStore = [
 	{ value: '1', label: 'Undergraduate Student' },
-	{ value: '2', label: 'Graduate Student' },
+	// { value: '2', label: 'Graduate Student' },
 	{ value: '3', label: 'Faculty' },
-	{ value: '4', label: 'Admin/REPS' },
-	{ value: '5', label: 'Alumni' }
+	{ value: '4', label: 'Admin' },
+	// { value: '5', label: 'Alumni' }
 ];
